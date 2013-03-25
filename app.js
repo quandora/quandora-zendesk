672b0fc87216c8383241938de494ddd301d0ecba
b6 <b6@quandora.com>
2013-03-25 01:11:16 -0400

    cleaner request / display, add quick search

--- app.js
+++ app.js
@@ -32,30 +32,27 @@
     },
 
     events: {
-      'app.activated': 'renderQuestions',
+      'app.activated': 'renderRelatedQuestions',
 
-      'click .back_to_list': 'renderQuestions',
+      'click .back_to_list': 'renderRelatedQuestions',
 
       //fetch question and display it
       'click .question': function(e) {
         var uuid = e.target.id;
-        this.ajax('fetchQuestion', uuid)
-        .done(function(response) {
-          this.viewQuestion(response.data);
-        });
+        this.renderQuestion(uuid);
       },
 
     },
 
-    domainUrl: function() {
+  domainUrl: function() {
       var url = this.setting('domainUrl');
       // remove trailing / if any
       var last = url.length - 1;
       if (url.indexOf('/', last) !== -1) { // ends with '/'
         url = url.substring(0, last);
-      }
+    }
     return url;
-    },
+  },
 
   computeBasicAuth: function() {
     var username = this.setting('username');
@@ -100,12 +97,16 @@
     return JSON.stringify({type: 'string', data: this.getMltQueryText()});
   },
 
-  viewQuestion: function(question) {
-    var domainUrl = this.domainUrl();
-    this.switchTo('question', {'question': question, 'domainUrl': domainUrl});
+  renderQuestion: function(uuid) {
+    this.ajax('fetchQuestion', uuid)
+      .done(function(response) {
+        var question = response.data;
+        var domainUrl = this.domainUrl();
+        this.switchTo('question', {'question': question, 'domainUrl': domainUrl});
+      });
   },
 
-  renderQuestions: function(questions) {
+  renderRelatedQuestions: function(questions) {
     this.ajax('fetchRelatedQuestions')
     .done(function(response) {
       var domainUrl = this.domainUrl();
