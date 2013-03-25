a0cb1462fd38a7918c6dc088ce97737a2b0cadcf
b6 <b6@quandora.com>
2013-03-25 00:14:57 -0400

    display question in sidebar, back to list link, more css work

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-'use strict';
-
 (function() {
 
   return {
     requests: {
-      fetchQuestions: function() {
+      fetchRelatedQuestions: function() {
         var postUrl = this.computeMltQuery();
         var auth = this.computeBasicAuth();
         var query = this.getMltQueryText();
@@ -18,19 +16,35 @@
           },
           headers: { 'Authorization': auth }
         };
+      },
+
+      fetchQuestion: function(uuid) {
+        var auth = this.computeBasicAuth();
+        var queryUrl = this.domainUrl() + '/m/json/q/' + uuid;
+        return {
+          url: queryUrl,
+          type: 'GET',
+          dataType: 'JSON',
+          headers: { 'Authorization': auth }
+        };
       }
+
     },
 
     events: {
-      'app.activated': 'requestQuestions',
-      'fetchQuestions.always': function(data) {
-        if (!data || data.type !== 'mlt') {
-          data = [];
-        } else {
-          data = data.data.result;
-        }
-        this.renderQuestions(data);
-      }
+      'app.activated': 'renderQuestions',
+
+      'click .back_to_list': 'renderQuestions',
+
+      //fetch question and display it
+      'click .question': function(e) {
+        var uuid = e.target.id;
+        this.ajax('fetchQuestion', uuid)
+        .done(function(response) {
+          this.viewQuestion(response.data);
+        });
+      },
+
     },
 
     domainUrl: function() {
@@ -40,61 +54,70 @@
       if (url.indexOf('/', last) !== -1) { // ends with '/'
         url = url.substring(0, last);
       }
-      return url;
+    return url;
     },
 
-    computeBasicAuth: function() {
-      var username = this.setting('username');
-      var password = this.setting('password');
-      return 'Basic ' + Base64.encode(username+':'+password);
-    },
+  computeBasicAuth: function() {
+    var username = this.setting('username');
+    var password = this.setting('password');
+    return 'Basic ' + Base64.encode(username+':'+password);
+  },
 
-    computeMltQuery: function() {
-      var url = this.domainUrl();
-      var kbase = this.setting('kbase');
-      if (kbase) {
-        url += '/m/json/kb/' + kbase + '/mlt';
-      } else {
-        url += '/m/json/mlt';
-      }
-      url += '?l=7';
-      return url;
-    },
+  computeMltQuery: function() {
+    var url = this.domainUrl();
+    var kbase = this.setting('kbase');
+    if (kbase) {
+      url += '/m/json/kb/' + kbase + '/mlt';
+    } else {
+      url += '/m/json/mlt';
+    }
+    url += '?l=7';
+    return url;
+  },
 
-    getMltQueryText: function() {
-      var text = '';
-      var ticket = this.ticket();
+  getMltQueryText: function() {
+    var text = '';
+    var ticket = this.ticket();
 
-      var subject = ticket.subject();
-      if (subject) {
-        text = subject;
-      }
-
-      var description = ticket.description();
-      if (description) {
-        text = text + ' ' + description;
-      }
-
-      var tags = ticket.tags();
-      if (tags) {
-        text = text + ' ' + tags.join(' ');
-      }
-      return text.trim();
-    },
-
-    computeQueryJsonMessage: function() {
-      return JSON.stringify({type: 'string', data: this.getMltQueryText()});
-    },
-
-    requestQuestions: function() {
-      this.ajax('fetchQuestions');
-    },
-
-    renderQuestions: function(questions) {
-      var domainUrl = this.domainUrl();
-      this.switchTo('list', {'questions': questions, 'domainUrl': domainUrl});
+    var subject = ticket.subject();
+    if (subject) {
+      text = subject;
     }
 
-  };
+    var description = ticket.description();
+    if (description) {
+      text = text + ' ' + description;
+    }
+
+    var tags = ticket.tags();
+    if (tags) {
+      text = text + ' ' + tags.join(' ');
+    }
+    return text.trim();
+  },
+
+  computeQueryJsonMessage: function() {
+    return JSON.stringify({type: 'string', data: this.getMltQueryText()});
+  },
+
+  viewQuestion: function(question) {
+    var domainUrl = this.domainUrl();
+    this.switchTo('question', {'question': question, 'domainUrl': domainUrl});
+  },
+
+  renderQuestions: function(questions) {
+    this.ajax('fetchRelatedQuestions')
+    .done(function(response) {
+      var domainUrl = this.domainUrl();
+      if (!response || response.type !== 'mlt') {
+        questions = [];
+      } else {
+        questions = response.data.result;
+      }
+      this.switchTo('list', {'questions': questions, 'domainUrl': domainUrl});
+    });
+  },
+
+};
 
 }());
